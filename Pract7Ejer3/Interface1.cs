efd15c980dfd8123347b8c2d32b7c12e9aef1329
DrEdgarvillegas <edgarvillegas5@gmail.com>
2022-05-10 10:45:50 -0300

    Pract7Ejer3

---
+++ Pract7Ejer3/Interface1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pract7Ejer3
+{
+    public interface IEmployee
+    {
+        string GetEmployeeDetails(int employeeId);
+    }
+
+    public interface IProject
+    {
+        string GetProjectDetails(int employeeId);
+    }
+}
