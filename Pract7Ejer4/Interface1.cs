f625739870d9789f5a49637de3a4568618f43ad5
DrEdgarvillegas <edgarvillegas5@gmail.com>
2022-05-10 10:55:40 -0300

    Pract7Ejer4ISP

---
+++ Pract7Ejer4/Interface1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pract7Ejer4
+{
+    public interface IEmployeeDatabase
+    {
+        bool AddEmployeeDetails();
+        bool ShowEmployeeDetails(int employeeId);
+    }
+
+    //-------------------------------------------------------------------------------
+
+    public interface IAddOperation
+    {
+        bool AddEmployeeDetails();
+    }
+    public interface IGetOperation
+    {
+        bool ShowEmployeeDetails(int employeeId);
+    }
+}
---
+++ Pract7Ejer4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pract7Ejer4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
