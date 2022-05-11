5822fed8bbc5eb01c59d2b75d77fed724a454ba3
DrEdgarvillegas <edgarvillegas5@gmail.com>
2022-05-11 13:00:22 -0300

    Pract8ejer1

---
+++ Pract8Ejer1/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pract8Ejer1
+{
+    public sealed class Singleton
+    {
+        private Singleton() { }
+
+        private static Singleton _countries;
+
+        private static Singleton _provinces;
+
+
+        public static Singleton getCountries() 
+    {
+        if (_countries == null) 
+        {
+            _countries = new Singleton();
+        }
+
+        return _countries;
+    }
+
+    
+
+    }
+}
